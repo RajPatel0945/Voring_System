35e1018c08812bb2eb287777439a266ad0e372a6
Raj Patel <pate0945@flinders.edu.au>
2022-09-13 20:53:29 +0930

    First commit

---
+++ src/test/java/com/flo/voring_system/VoringSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flo.voring_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VoringSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
